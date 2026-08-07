caaf7f68e7f8c13c691bcd2a31d9b18e5d78516e
ClaireVal <claire.valenchon@gmail.com>
2026-08-07 11:04:54 +0200

    Create Marche_aleatoire_1D_MSD_courtesVSlongue.py

---
+++ Marches_aléatoires/1D/Marche_aleatoire_1D_MSD_courtesVSlongue.py
@@ -0,0 +1,314 @@
+import numpy.random as rd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from scipy.optimize import curve_fit
+from math import exp
+
+#mpl.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+#mpl.rc('text', usetex=True)
+#mpl.rcParams['ytick.labelsize']=30
+#mpl.rcParams['xtick.labelsize']=30
+#mpl.rcParams['text.fontsize']=20
+mpl.rcParams['axes.labelsize']=35
+mpl.rcParams['legend.fontsize']=30
+mpl.rcParams['axes.titlesize']=35
+mpl.rcParams['figure.figsize']=(12,12)
+
+# Choix d'un p fixe
+p = 0.5
+# Initialisation des pas
+N= 3000
+M = 3000
+
+
+
+# Marche 1D d'une trajectoire de N pas réguliers
+
+positionsLongue = np.zeros(N)
+positionsLongue[0]=0
+x = 0
+
+for i in range(1, N):
+    u = rd.random()
+    if u<p:
+        positionsLongue[i]=x-1
+        x-=1
+    else:
+        positionsLongue[i]=x+1
+        x+=1
+
+
+# Marche de M trajectoires de N pas réguliers
+
+Mpositions = []
+
+for k in range(M):
+    positions = np.zeros(N)
+    positions[0]=0
+    x = 0
+    for i in range(1,N):
+        u = rd.random()
+        if u<p:
+            positions[i]=x-1
+            x-=1
+        else:
+            positions[i]=x+1
+            x+=1
+    Mpositions.append(np.copy(positions))
+
+
+
+
+###  Calcul du MSD  ###
+
+# Premier cas : Moyenne sur une grande trajectoire de M*N points
+
+def MSDLongue(x,dt):
+    n = N - dt                         #nombre de (t1,t2) tels que t2-t1=dt
+    sum1 = 0
+    for i in range(n):
+        sum1 += (x[i+dt]-x[i])**2
+    return sum1/n
+
+# Second cas : Moyenne sur M trajectoires
+
+def MSDCourtes(x, dt, M):
+    sum1 = 0
+    for i in range(M):                      #on parcourt les M trajectoires
+        sum1 += (x[i][dt]-x[i][0])**2       #on considère les x de la k-ième trajectoire
+    return sum1/M
+
+def fit_lineaire(x,y):
+    
+    S1 = float(len(y))
+    Sx = np.sum(x)
+    Sxx = np.sum(x**2)
+    Sxy = np.sum(x*y)
+    Sy = np.sum(y)
+    
+    delta = S1*Sxx-Sx**2
+    
+    b = ( Sy*Sxx-Sx*Sxy ) / delta 
+    a = ( S1*Sxy - Sx*Sy ) / delta
+    
+    d = a*x + b - y 
+    
+    sigma_y = np.sqrt( 1/(S1-2.0)*np.sum(d**2))
+    
+    sigma_a = np.sqrt(S1*sigma_y**2/delta)
+    
+    sigma_b = np.sqrt(Sxx*sigma_y**2/delta)
+    
+    return [a,sigma_a,b,sigma_b]
+
+
+# Définition des différents pas de temps
+dtLinListe = np.int_(np.linspace(0, N//10, 100))
+dtLogListe = np.unique(np.round(np.logspace(0, np.log10((N)//10), 200)).astype(int))
+    
+MSDLin_Long = np.zeros(len(dtLinListe))
+MSDLin_Cour = np.zeros(len(dtLinListe))
+MSDLog_Long = np.zeros(len(dtLogListe))
+MSDLog_Cour = np.zeros(len(dtLogListe))
+
+# Calcul des MSD correspondants
+for ind, l in enumerate(dtLinListe):
+    MSDLin_Long[ind] = MSDLongue(positionsLongue, l)
+for ind, c in enumerate(dtLinListe):
+    MSDLin_Cour[ind] = MSDCourtes(Mpositions, c, M)
+for ind, l in enumerate(dtLogListe):
+    MSDLog_Long[ind] = MSDLongue(positionsLongue, l)
+for ind, c in enumerate(dtLogListe):
+    MSDLog_Cour[ind] = MSDCourtes(Mpositions, c, M)
+    
+# np.savez("MSD.npz", dtLinListe_L = dtLinListe_L, dtLinListe_C=dtLinListe_C, dtLogListe_L = dtLogListe_L, dtLogListe_C=dtLogListe_C, MSDLin_Long=MSDLin_Long, MSDLin_Cour=MSDLin_Cour, stdLinLong=stdLinLong, stdLinCour=stdLinCour, MSDLog_Long=MSDLog_Long, MSDLog_Cour=MSDLog_Cour, stdLogLong=stdLogLong, stdLogCour=stdLogCour)
+
+
+
+# ### Estimation du coefficient de diffusion
+
+
+# Definition de la fonction linéaire
+def lineaire(t, y):
+    return t*y
+#Définition de la fonction affine
+def affine(t, x, y):
+    return x + y*t
+
+
+# Fitting sans pondération
+poptL1, _= curve_fit(lineaire, dtLinListe, MSDLin_Long)
+poptC1, _ = curve_fit(lineaire, dtLinListe, MSDLin_Cour)
+poptL2, _ = curve_fit(lineaire, dtLogListe, MSDLog_Long)
+poptC2, _ = curve_fit(lineaire, dtLogListe, MSDLog_Cour)
+
+print("D(long)=", poptL1[0]/2)
+print("D(cour)=", poptC1[0]/2)
+
+
+D_Log_L = exp(poptL2[0])/2
+D_Log_C = exp(poptC2[0])/2
+
+# fig, ax = plt.subplots()             
+# ax.plot(dtLinListe, lineaire(dtLinListe, *poptL1), ls="--", color="dodgerblue", label = "MSD long")
+# ax.plot(dtLinListe, MSDLin_Long, 'o', color="navy")
+# ax.plot(dtLinListe, lineaire(dtLinListe, *poptC1), ls="--", color="salmon", label= "MSD court")
+# ax.plot(dtLinListe, MSDLin_Cour, '^', color="orangered")
+# ax.set_title(f"Marche aléatoire selon une loi de Bernoulli de paramètre p={p} = positions trajectoire longue \n Dlong=poptL1[0]/2 et Dcourt=poptC1[0]/2", fontsize=16)
+# ax.set_xlabel("Position", fontsize=22)
+# ax.set_ylabel("Temps", fontsize=22)
+# mpl.rcParams['axes.labelsize']=22
+# mpl.rcParams['legend.fontsize']=22
+# mpl.rcParams['axes.titlesize']=22
+# mpl.rcParams['figure.figsize']=(12,12)
+# plt.tick_params(axis='both', labelsize=22)
+# plt.legend()
+# plt.show()  
+
+aL, sigmaaL, bL, sigmabL = fit_lineaire(dtLinListe, MSDLin_Long)
+aC, sigmaaC, bC, sigmabC = fit_lineaire(dtLinListe, MSDLin_Cour)
+print("Fit linéaire sur la trajectoire longue : a", aL, "sigmaa", sigmaaL, "b", bL, "sigmab", sigmabL)
+print("Fit linéaire sur les trajectoires courtes : a", aC, "sigmaa", sigmaaC, "b", bC, "sigmab", sigmabC)
+
+fig, ax = plt.subplots()             
+ax.plot(dtLogListe, lineaire(dtLogListe, *poptL2), ls="--", color="dodgerblue", label="Fit trajectoire longue")
+ax.plot(dtLogListe, MSDLog_Long, 'o', color="navy", label="Trajectoire longue")
+ax.plot(dtLogListe, lineaire(dtLogListe, *poptC2), ls="--", color="salmon", label="Fit trajectoires courtes")
+ax.plot(dtLogListe, MSDLog_Cour, '^', color="orangered", label="Trajectoires courtes")
+ax.set_title(f"Marche aléatoire selon une loi de Bernoulli de paramètre p={p}", fontsize=16)
+ax.set_xlabel("Position", fontsize=22)
+ax.set_ylabel("Temps", fontsize=22)
+mpl.rcParams['axes.labelsize']=22
+mpl.rcParams['legend.fontsize']=22
+mpl.rcParams['axes.titlesize']=22
+mpl.rcParams['figure.figsize']=(12,12)
+plt.legend(fontsize=10)
+plt.tick_params(axis='both', labelsize=22)
+plt.show() 
+
+# # Fitting avec pondération en 1/dt²
+# sigmaL = dtLinListe_L
+# sigmaC = dtLinListe_C
+# poptL2, _ = curve_fit(lineaire, dtLinListe_L, MSD_Long, sigma = sigmaL)
+# poptC2, _ = curve_fit(lineaire, dtLinListe_C, MSD_Cour, sigma = sigmaC)
+# mseL2 = mean_squared_error(MSD_Long, lineaire(dtLinListe_L, *poptL2))  
+# mseC2 = mean_squared_error(MSD_Cour, lineaire(dtLinListe_C, *poptC2))
+# print("MSE(trajectoire longue avec pondération) = ", mseL2)
+# print("MSE(trajectoires courtes avec pondération) = ", mseC2)
+
+# #Fitting avec pondération linéaire
+# # indices i = 0, 1, ..., N-1
+# iL = np.arange(0, len(dtLinListe_L), 1)
+# iC = np.arange(0, len(dtLinListe_C), 1)
+# # poids
+# wL = ((N*M) - dtLinListe_L[iL]) / ((N*M)*((N*M)-1)/2)
+# wC = (N - dtLinListe_C[iC]) / (N*(N-1)/2)
+# # conversion en sigma
+# sigmaLinL = 1 / np.sqrt(wL)
+# sigmaLinC = 1 / np.sqrt(wC)
+# poptL3, _ = curve_fit(lineaire, dtLinListe_L, MSD_Long, sigma = sigmaLinL)
+# poptC3, _ = curve_fit(lineaire, dtLinListe_C, MSD_Cour, sigma = sigmaLinC)
+
+# # Calcul du coefficient de diffusion D
+# D_L1 = poptL1/2
+# D_C1 = poptC1/2
+# D_L2 = poptL2/2
+# D_C2 = poptC2/2
+# D_L3 = poptL3/2
+# D_C3 = poptC3/2
+# print(" ")
+# print("Coefficient de diffusion cas trajectoire longue sans pondération : D_L = ", D_L1)
+# print("Coefficient de diffusion cas M trajectoires courtes sans pondération : D_C = ", D_C1)
+# print(" ")
+# print("Coefficient de diffusion cas trajectoire longue avec pondération : D_L = ", D_L2)
+# print("Coefficient de diffusion cas M trajectoires courtes avec pondération : D_C = ", D_C2)
+# print(" ")
+# print("Coefficient de diffusion théoriques : D_th = 0.5")
+
+
+# ### Tracés
+
+# # Tracé des MSD avec pondération quadratique
+# fig, ax = plt.subplots(2,2)
+# fig.suptitle(f"Comparaison des MSD longue trajectoire vs M petites trajectoires pour différents dt. N={N} et M={M}\n Pondération en 1/dt²", fontsize=16)
+# ax[0,0].plot(dtLinListe_L, MSD_Long, 'o',color = "blue")
+# ax[1,0].plot(dtLinListe_C, MSD_Cour, 'o',color = "red")
+# ax[0,0].set_xlabel("log(tau)", fontsize=14)
+# ax[0,0].set_ylabel("log(MSD)", fontsize=14)
+# ax[0,0].set_title(f"MSD de la longue trajectoire en fonction de tau\n D_L = {D_L1} sans pondération", fontsize=14)
+# ax[1,0].set_xlabel("log(tau)", fontsize=14)
+# ax[1,0].set_ylabel("log(MSD)", fontsize=14)
+# ax[1,0].set_title(f"MSD des courtes trajectoires en fonction de tau\n D_C = {D_C1} sans pondération", fontsize=14)
+# ax[0,0].tick_params(axis='both', labelsize=12)
+# ax[1,0].tick_params(axis='both', labelsize=12)
+# yFitL = poptL1*dtLinListe_L + poptL1
+# ax[0,0].plot(dtLinListe_L, yFitL, label="Courbe fittée trajectoire longue", color="cyan",ls='--',lw=2)
+# ax[1,0].plot(dtLinListe_C, lineaire(dtLinListe_C, *poptC1), label="Courbe fittée trajectoires courtes", color="darkorange",ls='--',lw=2)
+
+# ax[0,1].plot(dtLinListe_L, MSD_Long, 'o',color = "blue")
+# ax[1,1].plot(dtLinListe_C, MSD_Cour, 'o',color = "red")
+# ax[0,1].set_xlabel("log(tau)", fontsize=14)
+# ax[0,1].set_ylabel("log(MSD)", fontsize=14)
+# ax[0,1].set_title(f"MSD de la longue trajectoire en fonction de tau\n D_L = {D_L2} avec pondération", fontsize=14)
+# ax[1,1].set_xlabel("log(tau)", fontsize=14)
+# ax[1,1].set_ylabel("log(MSD)", fontsize=14)
+# ax[1,1].set_title(f"MSD des courtes trajectoires en fonction de tau\n D_C = {D_C2} avec pondération", fontsize=14)
+# ax[0,1].tick_params(axis='both', labelsize=12)
+# ax[1,1].tick_params(axis='both', labelsize=12)
+# yFitC = poptL2[1]*dtLinListe_L + poptL2[0]
+# ax[0,1].plot(dtLinListe_L, yFitC, label="Courbe fittée trajectoire longue", color="cyan",ls='--',lw=2)
+# ax[1,1].plot(dtLinListe_C, lineaire(dtLinListe_C, *poptC2), label="Courbe fittée trajectoires courtes", color="darkorange",ls='--',lw=2)
+
+# ax[0,0].set_xscale("log")
+# ax[0,0].set_yscale("log")
+# ax[1,0].set_xscale("log")
+# ax[1,0].set_yscale("log")
+# ax[0,1].set_xscale("log")
+# ax[0,1].set_yscale("log")
+# ax[1,1].set_xscale("log")
+# ax[1,1].set_yscale("log")
+# mpl.rcParams['axes.labelsize']=14
+
+# fig.tight_layout(rect=[0, 0, 1, 0.95])
+# fig.subplots_adjust(wspace=0.4, hspace = 0.5)
+
+
+# #Tracé pour la pondération linéaire
+# fig, ax = plt.subplots(2) 
+# ax[0].plot(dtLinListe_L, MSD_Long, 'o',color = "blue")
+# ax[0].set_xlabel("log(tau)", fontsize=14)
+# ax[0].set_ylabel("log(MSD)", fontsize=14)
+# ax[0].set_title(f"MSD de la longue trajectoire en fonction de tau\n D_L = {D_L3}\n avec pondération linéaire en 2(N-i)/((N-1)N)", fontsize=14)
+# ax[0].set_xscale("log")
+# ax[0].set_yscale("log")
+# yFit = poptL3[1]*dtLinListe_L + poptL3[0]
+# ax[0].plot(dtLinListe_L, yFit, label="Courbe fittée trajectoire longue", color="cyan",ls='--',lw=2)
+# ax[1].plot(dtLinListe_C, MSD_Cour, 'o',color = "blue")
+# ax[1].set_xlabel("log(tau)", fontsize=14)
+# ax[1].set_ylabel("log(MSD)", fontsize=14)
+# ax[1].set_title(f"MSD des courtes trajectoires en fonction de tau\n D_C = {D_C3}\n avec pondération linéaire en 2(N-i)/((N-1)N)", fontsize=14)
+# ax[1].set_xscale("log")
+# ax[1].set_yscale("log")
+# yFit = poptC3[1]*dtLinListe_C + poptC3[0]
+# ax[1].plot(dtLinListe_C, yFit, label="Courbe fittée trajectoires courtes", color="cyan",ls='--',lw=2)
+# ax[0].tick_params(axis='both', labelsize=12)
+# ax[1].tick_params(axis='both', labelsize=12)
+# fig.tight_layout(rect=[0, 0, 1, 0.95])
+# fig.subplots_adjust(hspace = 0.5)
+
+
+# # Tracé de la marche aléatoire
+# fig, ax = plt.subplots()             
+# ax.plot(positionsLongue, [i for i in range(N*M)])  
+# for k in range(M):
+#     ax.plot(Mpositions[k], [i for i in range(N)], alpha=0.5)
+# ax.set_title(f"Marche aléatoire selon une loi de Bernoulli de paramètre p={p} = positions trajectoire longue", fontsize=16)
+# ax.set_xlabel("Position", fontsize=14)
+# ax.set_ylabel("Temps", fontsize=14)
+# mpl.rcParams['axes.labelsize']=14
+# mpl.rcParams['legend.fontsize']=14
+# mpl.rcParams['axes.titlesize']=14
+# mpl.rcParams['figure.figsize']=(12,12)
+# plt.tick_params(axis='both', labelsize=14)
+# plt.show()                           
